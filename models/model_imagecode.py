803879f1c1be67dc45dc625b1a6d45cab1910f31
BennoKrojer <benno.krojer@gmail.com>
2022-08-04 11:42:20 -0400

    zeroshot ALBEF on imagecode

--- models/model_imagecode.py
+++ models/model_imagecode.py
@@ -59,7 +59,7 @@ class ALBEF(nn.Module):
         self.text_queue = nn.functional.normalize(self.text_queue, dim=0)
         
 
-    def forward(self, image, text, alpha, idx):
+    def forward(self, image, text):
         
         image_embeds = self.visual_encoder(image)
         image_atts = torch.ones(image_embeds.size()[:-1],dtype=torch.long).to(image.device)
@@ -69,101 +69,16 @@ class ALBEF(nn.Module):
                                         return_dict = True, mode = 'text')            
         text_embeds = text_output.last_hidden_state
         text_feat = F.normalize(self.text_proj(text_embeds[:,0,:]),dim=-1)                 
-
-        idx = idx.view(-1,1)
-        idx_all = torch.cat([idx.t(), self.idx_queue.clone().detach()],dim=1)  
-        pos_idx = torch.eq(idx, idx_all).float()       
-        sim_targets = pos_idx / pos_idx.sum(1,keepdim=True)     
-
-        with torch.no_grad():
-            self._momentum_update()
-            image_embeds_m = self.visual_encoder_m(image) 
-            image_feat_m = F.normalize(self.vision_proj_m(image_embeds_m[:,0,:]),dim=-1)  
-            image_feat_all = torch.cat([image_feat_m.t(),self.image_queue.clone().detach()],dim=1)                                         
-            text_output_m = self.text_encoder_m(text.input_ids, attention_mask = text.attention_mask,             
-                                                return_dict = True, mode = 'text')    
-            text_feat_m = F.normalize(self.text_proj_m(text_output_m.last_hidden_state[:,0,:]),dim=-1) 
-            text_feat_all = torch.cat([text_feat_m.t(),self.text_queue.clone().detach()],dim=1)
-
-            if self.distill:               
-                sim_i2t_m = image_feat_m @ text_feat_all / self.temp 
-                sim_t2i_m = text_feat_m @ image_feat_all / self.temp   
-
-                sim_i2t_targets = alpha * F.softmax(sim_i2t_m, dim=1) + (1 - alpha) * sim_targets
-                sim_t2i_targets = alpha * F.softmax(sim_t2i_m, dim=1) + (1 - alpha) * sim_targets 
-
-        sim_i2t = image_feat @ text_feat_all / self.temp 
-        sim_t2i = text_feat @ image_feat_all / self.temp           
-
-        if self.distill:
-            loss_i2t = -torch.sum(F.log_softmax(sim_i2t, dim=1)*sim_i2t_targets,dim=1).mean()
-            loss_t2i = -torch.sum(F.log_softmax(sim_t2i, dim=1)*sim_t2i_targets,dim=1).mean() 
-        else:
-            loss_i2t = -torch.sum(F.log_softmax(sim_i2t, dim=1)*sim_targets,dim=1).mean()
-            loss_t2i = -torch.sum(F.log_softmax(sim_t2i, dim=1)*sim_targets,dim=1).mean()   
-
-        loss_ita = (loss_i2t+loss_t2i)/2
-
-        self._dequeue_and_enqueue(image_feat_m, text_feat_m, idx)
-
-        ###=================================###
-        # forward the positve image-text pair
-        output_pos = self.text_encoder(encoder_embeds = text_embeds, 
+        vl = self.text_encoder(encoder_embeds = text_embeds, 
                                         attention_mask = text.attention_mask,
                                         encoder_hidden_states = image_embeds,
                                         encoder_attention_mask = image_atts,      
                                         return_dict = True,
                                         mode = 'fusion',
                                        )            
-        with torch.no_grad():
-            bs = image.size(0)      
-            weights_i2t = F.softmax(sim_i2t[:,:bs]+1e-4,dim=1)
-            weights_t2i = F.softmax(sim_t2i[:,:bs]+1e-4,dim=1)
-
-            mask = torch.eq(idx, idx.T)
-            weights_i2t.masked_fill_(mask, 0)
-            weights_t2i.masked_fill_(mask, 0) 
-
-        # select a negative image for each text
-        image_embeds_neg = []    
-        for b in range(bs):
-            neg_idx = torch.multinomial(weights_t2i[b], 1).item()
-            image_embeds_neg.append(image_embeds[neg_idx])
-        image_embeds_neg = torch.stack(image_embeds_neg,dim=0)   
-
-        # select a negative text for each image
-        text_embeds_neg = []
-        text_atts_neg = []
-        for b in range(bs):
-            neg_idx = torch.multinomial(weights_i2t[b], 1).item()
-            text_embeds_neg.append(text_embeds[neg_idx])
-            text_atts_neg.append(text.attention_mask[neg_idx])
-        text_embeds_neg = torch.stack(text_embeds_neg,dim=0)   
-        text_atts_neg = torch.stack(text_atts_neg,dim=0)      
-
-        text_embeds_all = torch.cat([text_embeds, text_embeds_neg],dim=0)     
-        text_atts_all = torch.cat([text.attention_mask, text_atts_neg],dim=0)     
-
-        image_embeds_all = torch.cat([image_embeds_neg,image_embeds],dim=0)
-        image_atts_all = torch.cat([image_atts,image_atts],dim=0)
-
-        output_neg = self.text_encoder(encoder_embeds = text_embeds_all, 
-                                        attention_mask = text_atts_all,
-                                        encoder_hidden_states = image_embeds_all,
-                                        encoder_attention_mask = image_atts_all,      
-                                        return_dict = True,
-                                        mode = 'fusion',
-                                       )                         
-
-        vl_embeddings = torch.cat([output_pos.last_hidden_state[:,0,:], output_neg.last_hidden_state[:,0,:]],dim=0)
+        vl_embeddings = vl.last_hidden_state[:,0,:]
         vl_output = self.itm_head(vl_embeddings)            
-
-        itm_labels = torch.cat([torch.ones(bs,dtype=torch.long),torch.zeros(2*bs,dtype=torch.long)],
-                               dim=0).to(image.device)
-        loss_itm = F.cross_entropy(vl_output, itm_labels)     
-
-        return loss_ita, loss_itm 
- 
+        return vl_output
 
 
     @torch.no_grad()    
